5708d6c4e6b2af81be7573803260edc75275c5c7
Jessica <jgomeznd@gmail.com>
2018-12-19 23:17:04 -0800

    grabs all of the games!

--- client/src/Game.js
+++ client/src/Game.js
@@ -4,63 +4,36 @@ import { address_getAll } from "./server";
 
 class Game extends React.Component {
   state = {
-    allInfo: [],
-    gameInfo: []
+    allInfo: []
   };
 
   componentDidMount = () => {
     address_getAll()
-      .then(response =>
-        this.setState({ allInfo: response }, this.createNewGameArray)
-      )
+      .then(response => this.setState({ allInfo: response }))
       .catch(error => console.error("game error===", error));
   };
 
-  createNewGameArray = () => {
-    let gameInfo = this.state.allInfo.map(info => {
-      console.log("embed value===", info.embedValue.substring(7).split(">")[0]);
-      let substring1 = info.embedValue.substring(7);
-      let substring2 = substring1.substring(
-        0,
-        info.embedValue.indexOf("flash")
-      );
-      console.log("embed try 2===", substring2);
-      return info.embedValue.substring(7).split(">")[0];
-    });
-    this.setState({ gameInfo }, this.printGames);
-  };
-
   printGames = () => {
-    console.log("game info===", this.state.gameInfo);
-
-    for (let i = 0; i < this.state.gameInfo.length; i++) {
-      console.log("this.state.gameInfo[i]", this.state.gameInfo[i]);
+    let gameInfo = this.state.allInfo.map(info => {
+      let embed = info.embedValue.substring(info.embedValue.indexOf("src") + 5);
+      let embedSrc = embed.substring(0, embed.indexOf('"'));
+      console.log("embedSrc===", embedSrc);
       return (
         <div>
-          <embed src={this.state.src} />
+          <h3>{info.gameTitle}</h3>
+          <br />
+          <div>
+            <embed
+              src={embedSrc}
+              width="800"
+              height="512"
+              type="application/x-shockwave-flash"
+            />
+          </div>
         </div>
       );
-    }
-    // let gameInfo = this.state.allInfo.map(info => {
-    //   console.log(info.embedValue.substring(7).split(">")[0]);
-    //   console.log(
-    //     "trying this out",
-    //     info.embedValue
-    //       .substring(7)
-    //       .info.embedValue.substring(0, info.embedValue.indexOf("></embed"))
-    //   );
-
-    //   return (
-    //     <div>
-    //       <h3>{info.gameTitle}</h3>
-    //       <br />
-    //       <div>
-    //         <embed {...this.state.address[i]} />
-    //       </div>
-    //     </div>
-    //   );
-    // });
-    // return gameInfo;
+    });
+    return gameInfo;
   };
 
   render() {
