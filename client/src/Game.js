498d59ec15acedf9a0e9e6da85ef0284dcff114a
Jessica <jgomeznd@gmail.com>
2018-12-19 20:51:33 -0800

    returning empty array

--- client/src/Game.js
+++ client/src/Game.js
@@ -10,62 +10,63 @@ class Game extends React.Component {
 
   componentDidMount = () => {
     address_getAll()
-      .then(response => this.setState({ allInfo: response }))
+      .then(response =>
+        this.setState({ allInfo: response }, this.createNewGameArray)
+      )
       .catch(error => console.error("game error===", error));
   };
 
   createNewGameArray = () => {
     let gameInfo = this.state.allInfo.map(info => {
-      console.log(info.embedValue.substring(7).split(">")[0]);
+      console.log("embed value===", info.embedValue.substring(7).split(">")[0]);
       return info.embedValue.substring(7).split(">")[0];
     });
     this.setState({ gameInfo }, this.printGames);
   };
 
   printGames = () => {
-    let gameInfo = this.state.allInfo.map(info => {
-      console.log(info.embedValue.substring(7).split(">")[0]);
-      console.log(
-        "trying this out",
-        info.embedValue
-          .substring(7)
-          .info.embedValue.substring(0, info.embedValue.indexOf("></embed"))
+    console.log("game info===", this.state.gameInfo);
+
+    for (let i = 0; i < this.state.gameInfo.length; i++) {
+      return (
+        <div>
+          <embed {...this.state.gameInfo[i]} />
+        </div>
       );
+    }
+    // let gameInfo = this.state.allInfo.map(info => {
+    //   console.log(info.embedValue.substring(7).split(">")[0]);
+    //   console.log(
+    //     "trying this out",
+    //     info.embedValue
+    //       .substring(7)
+    //       .info.embedValue.substring(0, info.embedValue.indexOf("></embed"))
+    //   );
 
-      for (let i = 0; i < this.state.allInfo.length; i++) {
-        let embed = info.embedValue.substring(7).split(">")[0];
-
-        return (
-          <div>
-            <h3>{info.gameTitle}</h3>
-            <br />
-            <div>
-              <embed {...this.state.address[i]} />
-              {/* ) +
-              info.embedValue.substring(
-                0,
-                info.embedValue.indexOf("></embed>")
-              ) +
-              "/>"} */}
-            </div>
-          </div>
-        );
-      }
-    });
-    return gameInfo;
+    //   return (
+    //     <div>
+    //       <h3>{info.gameTitle}</h3>
+    //       <br />
+    //       <div>
+    //         <embed {...this.state.address[i]} />
+    //       </div>
+    //     </div>
+    //   );
+    // });
+    // return gameInfo;
   };
 
   render() {
     let printGames = this.printGames();
     return (
       <div>
-        <embed
+        {/* <embed
           width="800"
           height="512"
           base="https://external.kongregate-games.com/gamez/0021/0593/live/"
           src="https://external.kongregate-games.com/gamez/0021/0593/live/embeddable_210593.swf"
           type="application/x-shockwave-flash"
-        />
+        /> */}
         <br />
         <div>{printGames}</div>
       </div>
